b2aaaf1a142a0dcf63b278a81a5e94bd53122c76
mrog357 <84156845+mrog357@users.noreply.github.com>
2022-06-17 09:28:40 +0200

    Add files via upload

--- projekt.py
+++ projekt.py
@@ -1,3 +1,4 @@
+import math
 import tkinter
 from cgitb import text
 from tkinter import *
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from scipy import signal
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,  NavigationToolbar2Tk
 from matplotlib.pyplot import Figure
 import random
 from matplotlib import style
@@ -50,17 +51,21 @@ photo = PhotoImage(file='układ.gif')
 amplitude = 1
 freq = 1
 step = 0.001
-end_time = 10
-signal_type = 3
-duty = 0.5
-impulse_num = 10
+end_time = 4
+signal_type = 1
+duty = 1
+impulse_num = 1
 R2 = 5
 R1 = 10
-C = 0.001
+C = 1
 
 input_tab = []
 output_tab = []
 time_tab = []
+pulse_tab = []
+bode_amplitude_tab = []
+bode_phase_tab = []
+bode_bounds = {-500, 500}
 
 
 def count():
@@ -70,9 +75,13 @@ def count():
     #    R1 = int(textfield[0].get("1.0",END))
     #   print(R1)
 
+    clear_tabs()
     fill_time_tab()
-    fill_input_tab(2)
+    fill_input_tab(signal_type)
     fill_output_tab()
+    fill_pulse_tab()
+    fill_bode_phase_tab()
+    fill_bode_amplitude_tab()
     plot()
 
 
@@ -198,14 +207,68 @@ def fill_input_tab(type):
         case 3:
             for t in time_tab:
                 input_tab.append(triang_sig(t))
+def extend_input_tab(type, start, stop, step2, extended_input_tab):
+    t = start
+    stop =int( (stop - start)/step2)
+    match type:
+        case 1:
+            for i in range(0, stop):
+                t += step2
+                extended_input_tab.append(square_sig(t))
+        case 2:
+            for t in range(0, stop):
+                t += step2
+                extended_input_tab.append(sin_sig(t))
+        case 3:
+            for t in range(0, stop):
+                t += step2
+                extended_input_tab.append(triang_sig(t))
 
 def fill_output_tab():
     ur = 0.0
-    for u1 in input_tab:
-        ur = ur + step * (u1 - ur*(1+(R2/R1))/C*R2)
+
+    for i in range(0, time_tab.__len__()) :
+        u1 = input_tab[i]
         output_tab.append(ur)
 
+        diff = u1/C - ((1+(R2/R1))/C)*ur
+        ur = ur + step*diff
 
+
+
+
+        # if(i<400):
+        #     print(time_tab[i])
+        #     print(u1)
+        #     print(ur)
+        #     print(diff)
+        #     print("    ,     ")
+
+def fill_pulse_tab():
+    for i in range ( -200, 200):
+        pulse_tab.append(pow(1.2, i))
+
+
+def fill_bode_amplitude_tab():
+    for w in pulse_tab:
+        a = 1/math.sqrt(pow((1 + R2/R1), 2)+pow(C*w, 2))
+        a_dB = 20*math.log(a, 10)
+        bode_amplitude_tab.append(a_dB)
+        print(str(w) + "         " + str(a_dB))
+
+def fill_bode_phase_tab():
+    for w in pulse_tab:
+        phi = math.atan2(0, 1) - math.atan2(C*w, (1 + R2/R1))
+        bode_phase_tab.append(phi)
+
+
+def clear_tabs():
+    time_tab.clear()
+    output_tab.clear()
+    input_tab.clear()
+    pulse_tab.clear()
+    bode_amplitude_tab.clear()
+    bode_phase_tab.clear()
 
 def plot():
     print("working")
@@ -217,7 +280,8 @@ def plot():
     fig = Figure(figsize=(5,5), dpi=100, facecolor="gray")
     ax1 = fig.add_subplot(221)
     ax2 = fig.add_subplot(222)
-    ax3 = fig.add_subplot(212)
+    ax3 = fig.add_subplot(223)
+    ax4 = fig.add_subplot(224)
 
     # ax1 = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1)
     # ax2 = plt.subplot2grid((6, 1), (0, 0), rowspan=3, colspan=1)
@@ -233,10 +297,12 @@ def plot():
     xs.append(x)
     ys.append(y)
 
-
-    ax1.plot(x,y)
+    ax1.plot(time_tab, input_tab)
     ax2.plot(time_tab, output_tab)
-    ax3.plot(time_tab, output_tab)
+    ax3.plot(pulse_tab, bode_amplitude_tab)
+    ax4.plot(pulse_tab, bode_phase_tab)
+    ax3.set_xscale('log')
+    ax4.set_xscale('log')
 
     # create matplotlib canvas using `fig` and assign to widget `top`
     canvas = FigureCanvasTkAgg(fig, charts)
